a9ace7881970f1cea9c6a454b1a819f9f68a9826
CalArcher <p.calarch@gmail.com>
2022-10-26 14:23:28 -0600

    weight converter

--- python-weight-converter/app.py
+++ python-weight-converter/app.py
@@ -2,8 +2,9 @@ weight = input('Enter Weight: ')
 typeOfWeight = input ('Kg? Type "k" or Lbs? Type "l": ')
 
 weight = int(weight)
+typeOfWeight = typeOfWeight.lower()
 
 if typeOfWeight == 'k':
-  print(round(weight * 2.204))
+  print('weight in lbs: ' + str(round(weight / 0.45)))
 elif typeOfWeight == 'l':
-  print(weight // 2.204)
+  print('weight in kgs: ' + str(round(weight * .45)))
